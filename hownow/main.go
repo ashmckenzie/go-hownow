60195a8abcf4b7e685e4799319e432924ba69d73
Ash McKenzie <ash@the-rebellion.net>
2017-07-26 15:59:43 +1000

    WIP

--- hownow/main.go
+++ hownow/main.go
@@ -5,6 +5,11 @@ import (
 	"os"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
+	"github.com/olebedev/when"
+	"github.com/olebedev/when/rules/common"
+	"github.com/olebedev/when/rules/en"
+
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -13,6 +18,9 @@ var (
 	noNewLine = app.Flag("no-new-line", "Don't print a newline at the end").Short('n').Bool()
 	epoch     = app.Flag("epoch", "Format as seconds since epoch").Short('e').Bool()
 	offset    = app.Flag("offset", "Offset now").Int()
+
+	free     = app.Command("free", "Free form")
+	freeText = free.Arg("text", "Free form text").Required().String()
 
 	bod = app.Command("bod", "Beginning of day").Alias("sod")
 	eod = app.Command("eod", "End of day")
@@ -28,6 +36,18 @@ func main() {
 	fmt.Printf(parse(os.Args[1:]))
 }
 
+func whenTest(text string) {
+	w := when.New(nil)
+	w.Add(en.All...)
+	w.Add(common.All...)
+
+	r, err := w.Parse(text, time.Now())
+	spew.Dump(err)
+	if err == nil {
+		spew.Dump(r.Time)
+	}
+}
+
 func parse(args []string) string {
 	var theTime string
 	app.Version(version)
@@ -35,6 +55,8 @@ func parse(args []string) string {
 	hownow := New(time.Now(), *offset)
 
 	switch kingpin.MustParse(app.Parse(args)) {
+	case free.FullCommand():
+		whenTest(*freeText)
 	case bod.FullCommand():
 		theTime = hownow.Process("bod", *epoch)
 	case eod.FullCommand():
